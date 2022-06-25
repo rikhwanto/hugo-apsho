de6e90449e6c390d7850a5baf1005e469344443b
Ridhwan Ikhwanto <ridhwan@ikhwanto.com>
2022-06-25 14:44:02 +0700

    Switch form handler to netlify

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -13,29 +13,4 @@ window.addEventListener("DOMContentLoaded", function () {
     status.innerHTML = "Oops! There was a problem.";
   }
 
-  // handle the form submission event
-  if (form != null) {
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  }
 });
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
